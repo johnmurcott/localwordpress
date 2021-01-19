7ab86077e085171b8f5cd622a92a2bfe9a7fe91c
johnmurcott <77120308+johnmurcott@users.noreply.github.com>
2021-01-19 13:16:57 -0500

    My first change to 404 page
    Just for testing

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@ get_header();
 
 		<h1 class="entry-title"><?php _e( 'Page Not Found', 'twentytwenty' ); ?></h1>
 
-		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
+		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. John was here. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
 
 		<?php
 		get_search_form(
